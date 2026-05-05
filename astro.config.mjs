dfd1d723d873989e51767562ccc5a79327888d9b
Eduard Lostak <103068736+Eddlost@users.noreply.github.com>
2026-05-05 14:09:17 +0200

    Configure site and base for GitHub Pages deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,8 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
-  site: 'https://lospro.github.io',
-  base: '/',
+  site: 'https://eddlost.github.io',
+  base: '/LOSPRO_WEB',
   integrations: [tailwind()],
   output: 'static',
 });
